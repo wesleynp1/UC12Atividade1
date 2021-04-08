7b9fd560fe9cfb37a12d0af692b264b7d3be200c
Wesley <wesleynp987@gmail.com>
2021-04-08 12:14:45 -0300

    Scroll Infinito

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -41,6 +41,24 @@ export default class MyList extends Component {
               text: "Proin hendrerit nisl id turpis bibendum, sit amet scelerisque augue elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a blandit sapien.",
               image: require("../images/img3.jpg"),
               id: this.state.dados.length+2
+            },
+            {
+              title: "Lorem ipsum", 
+              text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in congue risus, non viverra tellus. Nam faucibus ligula non metus ultrices mollis. Cras dolor purus, hendrerit eu eros quis, dignissim eleifend mi. In tincidunt mi in diam egestas congue ac ut purus. Nulla semper libero vitae blandit vehicula.",
+              image: require("../images/img1.jpg"),
+              id: this.state.dados.length+3
+            },
+            {
+              title: "Curabitur vulputate", 
+              text: "Curabitur vulputate enim in lacus imperdiet, a convallis odio posuere. Nulla id ex et purus sodales rutrum non eu eros. Ut consequat est lacus.",
+              image: require("../images/img2.jpg"),
+              id: this.state.dados.length+4
+            },
+            {
+              title: "Proin hendrerit", 
+              text: "Proin hendrerit nisl id turpis bibendum, sit amet scelerisque augue elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a blandit sapien.",
+              image: require("../images/img3.jpg"),
+              id: this.state.dados.length+5
             }];
 
             if(this.componenteRederizado)
@@ -83,6 +101,8 @@ export default class MyList extends Component {
             data={this.state.dados}
             renderItem={this.renderDados}
             keyExtractor={(item)=> item.id.toString()}
+            onEndReached={()=>{this.getListOfData()}}
+            onEndReachedThreshold={0.1}
             />
           );}
       }
